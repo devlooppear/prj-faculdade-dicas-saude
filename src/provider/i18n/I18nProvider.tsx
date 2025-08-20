1d683e773d24dc43a00a05bc424bbd5718682f14
devlooppear <iago.profissional.developer@gmail.com>
2025-08-20 20:31:13 -0300

    fix build

--- src/provider/i18n/I18nProvider.tsx
+++ src/provider/i18n/I18nProvider.tsx
@@ -1,9 +1,25 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import React, { useEffect, useState } from "react";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
 
-export default function I18nProvider({ children }: { children: React.ReactNode }) {
+export default function I18nProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    if (!i18n.isInitialized) {
+      i18n.init({}).then(() => setReady(true));
+    } else {
+      setReady(true);
+    }
+  }, []);
+
+  if (!ready) return null;
+
   return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 }
